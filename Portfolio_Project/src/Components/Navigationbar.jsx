def20ddeecd188d6833e4da7b5c38ab8b0be4027
Adrian Botello <adrianbotello1234@gmail.com>
2025-03-30 21:23:39 -0700

    Finished structure

---
+++ Portfolio_Project/src/Components/Navigationbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../Styles/NavBar.css'
+import reactLogo from '../assets/react.svg'
+function NavigationBar() {
+
+  return (
+    <div>
+        <nav className="navbar">
+            <ul className="nav-Sections">
+                <li><a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+                </a></li>
+                <li className="linked"><a href= "#Projects">Projects</a></li>
+                <li className="linked"><a href= "#About Me">About Me</a></li>
+                <li className="linked"><a href= "#All tools used">All tools used</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavigationBar
---
+++ Portfolio_Project/src/Pages/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../Styles/Styles.css'
+import NavigationBar from '../Components/Navigationbar.jsx'
+function App() {
+
+  return (
+    <>
+      <NavigationBar />
+    </>
+  )
+}
+
+export default App
